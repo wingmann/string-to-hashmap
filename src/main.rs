1486ac48bbb6f0232c6593d368e89a1c76fd353f
Alexander Shavrov <alexander.shavrov@outlook.com>
2022-08-28 05:31:04 +0300

    Logic is divided into corresponding functions

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,21 @@ fn main() {
         key2: value2
         // Another comment
         key3: value3
-        "#
-    .to_string();
+        "#;
 
+    let hashmap = translate_to_hashmap(row_string.to_string());
+
+    for (key, value) in hashmap.iter() {
+        println!("{}: {}", key, value);
+    }
+}
+
+fn translate_to_hashmap(row_string: String) -> HashMap<String, String> {
+    let mut lines = get_filtered_lines(&row_string);
+    get_hashmap_from(&mut lines)
+}
+
+fn get_filtered_lines(row_string: &String) -> Vec<String> {
     let mut lines = vec![];
 
     for line in row_string
@@ -20,18 +32,25 @@ fn main() {
     {
         lines.push(line.trim().to_string());
     }
-    let mut result = HashMap::<String, String>::new();
+    lines
+}
+
+fn get_hashmap_from(lines: &mut Vec<String>) -> HashMap<String, String> {
+    let mut hashmap = HashMap::<String, String>::new();
 
     for line in lines.iter() {
-        let v: Vec<&str> = line.split(":").collect();
-        let a = match v.get(0) {
-            Some(key) => key,
-            None => "",
-        };
-        let b = match v.get(1) {
-            Some(val) => val,
-            None => "",
-        };
-        result.insert(a.trim().to_string(), b.trim().to_string());
+        let pair: Vec<&str> = line.split(":").collect();
+
+        hashmap.insert(
+            match pair.get(0) {
+                Some(key) => key.trim().to_string(),
+                None => "".to_string(),
+            },
+            match pair.get(1) {
+                Some(value) => value.trim().to_string(),
+                None => "".to_string(),
+            },
+        );
     }
+    hashmap
 }
